b11d994f8265d2fd72c7005c112024268698e2c1
Youngin Shin <100068912+shinyoungin4137@users.noreply.github.com>
2022-08-05 00:19:09 +0900

    Add files via upload
    2775

--- baekjoon2.cpp
+++ baekjoon2.cpp
@@ -1,22 +1,18 @@
-//10250
+//2775
 #include <iostream>
 using namespace std;
-void showData(int, int, int);
+int getNum(int, int);
 int main() {
-	int t, h, w, n,cnt=0;
+	int t, k, n;
 	cin >> t;
 	for (int i = 0; i < t; i++) {
-		cin >> h >> w >> n;
-		showData(h, w, n);
+		cin >> k >> n;
+		cout << getNum(k, n) << "\n";
 	}
 	return 0;
 }
-void showData(int h, int w, int n) {
-	int cnt = 0;
-	for (int j = 0; j < w; j++)
-		for (int k = 0; k < h; k++) {
-			if (++cnt == n)
-				cout << (k + 1) * 100 + j + 1 << endl;
-		}//위로부터 올라감
-
+int getNum(int k, int n) {
+	if (k == 0) return n;
+	if (n == 1) return n;
+	return getNum(k, n - 1) + getNum(k - 1, n);
 }
